9cda7dddbc8276d217906ef22881e3eb587eb96a
cuefront <131971014+dayview@users.noreply.github.com>
2025-11-15 23:48:12 +0800

    feat(main+login): implementation of ui responsiveness in controller, and relocation of new login window to loginview.fxml

--- app/src/main/java/businesspermitsystem/MainApp.java
+++ app/src/main/java/businesspermitsystem/MainApp.java
@@ -15,7 +15,7 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/view/SQLLoginView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/LoginView.fxml"));
         
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("/styles/main-theme.css").toExternalForm());
