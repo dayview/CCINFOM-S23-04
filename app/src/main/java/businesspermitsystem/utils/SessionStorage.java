b40c42e8b810fe6a0bfb6a6c49c33febe28c4b28
antqnluis <antqnluis@gmail.com>
2025-11-17 23:37:24 +0800

    added functionality to register new business, or choose from business, register a new owner, or choose from a owner then link the two

--- app/src/main/java/businesspermitsystem/utils/SessionStorage.java
+++ app/src/main/java/businesspermitsystem/utils/SessionStorage.java
@@ -2,15 +2,12 @@ package businesspermitsystem.utils;
 
 import businesspermitsystem.models.BusinessModel;
 import businesspermitsystem.models.OwnerModel;
-import businesspermitsystem.models.PermitTypeModel;
 
 public class SessionStorage {
 
     private static BusinessModel selectedBusiness;
     private static OwnerModel selectedOwner;
-    private static PermitTypeModel selectedPermitType;
 
-    // --- BUSINESS ---
     public static void setSelectedBusiness(BusinessModel business) {
         selectedBusiness = business;
     }
@@ -19,7 +16,6 @@ public class SessionStorage {
         return selectedBusiness;
     }
 
-    // --- OWNER ---
     public static void setSelectedOwner(OwnerModel owner) {
         selectedOwner = owner;
     }
@@ -28,19 +24,8 @@ public class SessionStorage {
         return selectedOwner;
     }
 
-    // --- PERMIT TYPE ---
-    public static void setSelectedPermitType(PermitTypeModel permitType) {
-        selectedPermitType = permitType;
-    }
-
-    public static PermitTypeModel getSelectedPermitType() {
-        return selectedPermitType;
-    }
-
-    // --- CLEAR SESSION ---
     public static void clear() {
         selectedBusiness = null;
         selectedOwner = null;
-        selectedPermitType = null;
     }
 }
