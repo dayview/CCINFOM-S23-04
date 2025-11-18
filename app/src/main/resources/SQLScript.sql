a8e8bea205f32131a2a708dad2559c61ec06094f
antqnluis <antqnluis@gmail.com>
2025-11-18 19:15:48 +0800

    added my tables to main SQLscript

--- app/src/main/resources/SQLScript.sql
+++ app/src/main/resources/SQLScript.sql
@@ -5,15 +5,19 @@
 -- Password: <depending on user settings>
 USE `business_database`;
 
-DROP TABLE IF EXISTS `inspection_result`;
-DROP TABLE IF EXISTS `inspection_handle`;
-DROP TABLE IF EXISTS `permit_application`;
-DROP TABLE IF EXISTS `business_owner`;
-DROP TABLE IF EXISTS `business`;
-DROP TABLE IF EXISTS `owner`;
-DROP TABLE IF EXISTS `inspector`;
-DROP TABLE IF EXISTS `permit_type`;
-DROP TABLE IF EXISTS `municipality`;
+DROP TABLE IF EXISTS payment;
+DROP TABLE IF EXISTS inspection_schedule;
+DROP TABLE IF EXISTS permit_application;
+DROP TABLE IF EXISTS business_owner;
+
+DROP TABLE IF EXISTS business;
+DROP TABLE IF EXISTS owner;
+DROP TABLE IF EXISTS inspector;
+DROP TABLE IF EXISTS inspection_result;
+DROP TABLE IF EXISTS inspection_handle;
+DROP TABLE IF EXISTS permit_type;
+
+DROP TABLE IF EXISTS municipality;
 
 CREATE TABLE `municipality` (
   `municipality_id` INT NOT NULL AUTO_INCREMENT,
@@ -125,6 +129,23 @@ CREATE TABLE `inspection_schedule` (
   FOREIGN KEY (`inspector_id`) REFERENCES `inspector`(`inspector_id`),
   FOREIGN KEY (`business_id`) REFERENCES `business`(`business_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE payment (
+    payment_id INT NOT NULL AUTO_INCREMENT,
+    application_id INT NOT NULL,
+    business_id INT NOT NULL,
+    permit_type_id INT NOT NULL,
+    municipality_id INT NOT NULL,
+    payment_date DATE NOT NULL,
+    amount_paid DECIMAL(10,2) NOT NULL,
+    mode_of_payment VARCHAR(50),
+    or_number VARCHAR(50),
+    PRIMARY KEY (payment_id),
+    FOREIGN KEY (application_id) REFERENCES permit_application(application_id),
+    FOREIGN KEY (business_id) REFERENCES business(business_id),
+    FOREIGN KEY (permit_type_id) REFERENCES permit_type(permit_type_id),
+    FOREIGN KEY (municipality_id) REFERENCES municipality(municipality_id)
+);
 
 INSERT INTO `municipality`
 (`municipality_name`, `province`, `region`, `classification`, `contact_number`, `office_street`, `office_barangay`, `office_zipcode`)
