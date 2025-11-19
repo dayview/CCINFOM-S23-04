e3c36757d7de01b666f9639554f1a5638756fbb3
antqnluis <antqnluis@gmail.com>
2025-11-19 18:14:44 +0800

    fixed naming issues

--- app/src/main/java/businesspermitsystem/models/InitialPermitTypeModel.java
+++ app/src/main/java/businesspermitsystem/models/InitialPermitTypeModel.java
@@ -10,18 +10,20 @@ public class InitialPermitTypeModel {
     private String surchargeRule;
     private int validityMonths;
     private String documentRequirements;
+    private int feeScheduleId;
 
     public InitialPermitTypeModel(int permitTypeId, String permitName, BigDecimal baseFee,
-                                  String surchargeRule, int validityMonths, String documentRequirements) {
+                                  String surchargeRule, int validityMonths,
+                                  String documentRequirements, int feeScheduleId) {
         this.permitTypeId = permitTypeId;
         this.permitName = permitName;
         this.baseFee = baseFee;
         this.surchargeRule = surchargeRule;
         this.validityMonths = validityMonths;
         this.documentRequirements = documentRequirements;
+        this.feeScheduleId = feeScheduleId;
     }
 
-    // Getters
     public int getPermitTypeId() {
         return permitTypeId;
     }
@@ -46,7 +48,10 @@ public class InitialPermitTypeModel {
         return documentRequirements;
     }
 
-    // Setters
+    public int getFeeScheduleId() {
+        return feeScheduleId;
+    }
+
     public void setPermitTypeId(int permitTypeId) {
         this.permitTypeId = permitTypeId;
     }
@@ -69,6 +74,10 @@ public class InitialPermitTypeModel {
 
     public void setDocumentRequirements(String documentRequirements) {
         this.documentRequirements = documentRequirements;
+    }
+
+    public void setFeeScheduleId(int feeScheduleId) {
+        this.feeScheduleId = feeScheduleId;
     }
 
     @Override
